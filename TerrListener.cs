0c752137c34c383148d9013e0995c43450443048
Justas Dabrila <justasdabrila@gmail.com>
2016-01-20 23:27:28 +0200

    Initial Commit.
    We have connection to a Terraria156 server but the connected user will
    somehow stop others from picking up items dropped by other users.

---
+++ TerrListener.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaBridge
+{
+    public class TerrariaLisener : IDisposable
+    {
+        private Socket Socket { get; } = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private string Host { get; }
+        private int Port { get; }
+        //private Guid Guid { get; }
+
+        private const int BufferSize = 131070;
+
+        private bool _canReceive = true;
+
+        private byte _playerId;
+
+        public TerrariaLisener(string host, int port /*, Guid guid */)
+        {
+            Host = host;
+            Port = port;
+            //Guid = guid;
+        }
+
+        private async Task OnReceivePacket(TerrPacket packet)
+        {
+            if (packet == null) return;
+
+            Console.WriteLine($"Received {packet.Type} of length {packet.Length}");
+
+            switch (packet.Type)
+            {
+                case TerrPacketType.ContinueConnecting:
+                    _playerId = packet.Payload[0];
+                    Console.WriteLine($"Set pid to {_playerId}");
+
+                    // player appearance
+                    Send(TerrPacket.Create(TerrPacketType.PlayerAppearance, new byte[]
+                    {
+                        _playerId, 0x00, 0x00, 0x04, 0x6e, 0x6f, 0x6e, 0x6f, 0x00, 0x00, 0x00, 0x00, 0xd7, 0x5a, 0x37,
+                        0xff, 0x7d, 0x5a, 0x69, 0x5a, 0x4b, 0xaf, 0xa5, 0x8c, 0xa0, 0xb4, 0xd7, 0xff, 0xe6, 0xaf, 0xa0,
+                        0x69, 0x3c, 0x00
+                    }));
+
+                    // send uuid
+                    //Send(TerrPacket.Create(PacketType.ClientUuid, new byte[] { 0x24, 0x30 }.Concat(Encoding.ASCII.GetBytes(Guid.ToString())).ToArray()));
+
+                    // send player life
+                    Send(TerrPacket.Create(TerrPacketType.PlayerLife, new byte[]
+                    {
+                        _playerId,
+                        0xff, 0x00, //current life (int16) 0x64
+                        0xff, 0x00 // max life (int16)
+                    }));
+
+                    // send player mana
+                    Send(TerrPacket.Create(TerrPacketType.PlayerMana, new byte[]
+                    {
+                        _playerId,
+                        0x14, 0x00, //current mana (int16)
+                        0x14, 0x00 // max mana (int16)
+                    }));
+                    // send player buffs
+                    Send(TerrPacket.Create(TerrPacketType.UpdatePlayerBuff, new byte[]
+                    {
+                        _playerId,
+                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                    }));
+
+                    // send inventory data
+                    for (byte i = 0; i <= 179; i++)
+                    {
+                        Send(TerrPacket.Create(TerrPacketType.SetInventory, new byte[]
+                        {
+                            i, _playerId,
+                            0x00, 0x00, // stack (int16)
+                            0x00, //prefix (byte)
+                            0x00, 0x00 //item netid (int16)
+                        }));
+                    }
+
+                    Send(TerrPacket.Create(TerrPacketType.RequestWorldInformation));
+                    break;
+                case TerrPacketType.WorldInformation: //WorldData
+
+                    Send(TerrPacket.Create(TerrPacketType.RequestInitialTileData, new byte[]
+                    {
+                        0xff, 0xff, 0xff, 0xff, // X (int32)
+                        0xff, 0xff, 0xff, 0xff // Y (int32)
+                    }));
+
+                    await Task.Delay(500);
+
+                    Send(TerrPacket.Create(TerrPacketType.SpawnPlayer, new byte[]
+                    {
+                        _playerId,
+                        0xff, 0xff, //spawn x (int16)
+                        0xff, 0xff //spawn y (int16)
+                        //0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff // starts in player id
+                    }));
+                    break;
+                //case PacketType.CompleteConnectionAndSpawn:
+                //    Send(TerrPacket.Create(PacketType.SpawnPlayer, new byte[]
+                //    {
+                //        _playerId,
+                //        0xff, 0xff, //spawn x (int16)
+                //        0xff, 0xff //spawn y (int16)
+                //    }));
+                //    break;
+                case TerrPacketType.ChatMessage:
+                    Console.WriteLine($"Chat message: {Encoding.ASCII.GetString(packet.Payload)}");
+                    break;
+            }
+            // todo: send keepalives
+        }
+
+        public async Task Start()
+        {
+            Socket.BeginConnect(Host, Port, (ar) => Socket.EndConnect(ar), null);
+
+            while (!Socket.Connected) await Task.Delay(100);
+            Console.WriteLine($"Socket connected to {Socket.RemoteEndPoint}");
+
+            Send(TerrPacket.ConnectPacket);
+
+            while (true)
+            {
+                if (!_canReceive)
+                    await Task.Delay(100);
+                else
+                    Receive();
+            }
+        }
+
+        private void Receive()
+        {
+            byte[] buffer = new byte[BufferSize];
+
+            _canReceive = false;
+            Socket.BeginReceive(buffer, 0, BufferSize, 0, ReceiveCallback, buffer);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            int bytesRead = Socket.EndReceive(ar);
+
+            if (bytesRead <= 0)
+            {
+                _canReceive = true;
+                return;
+            }
+
+            byte[] data = new byte[bytesRead];
+            Array.Copy((byte[]) ar.AsyncState, data, bytesRead);
+
+            Task.Run(async () => await OnReceivePacket(TerrPacket.Parse(data)));
+            _canReceive = true;
+        }
+
+        private void Send(byte[] data)
+        {
+            Socket.BeginSend(data, 0, data.Length, 0, (ar) => Socket.EndSend(ar), null);
+        }
+
+        public void Dispose()
+        {
+            Socket.Dispose();
+        }
+    }
+}
---
+++ TerrPacket.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+
+namespace TerrariaBridge
+{
+    public class TerrPacket
+    {
+        private const byte Index_Length = 0;
+        private const byte Index_PacketId = 2;
+        private const byte Index_Payload = 3;
+
+        private const byte Size_Length = sizeof(ushort);
+        private const byte Size_PacketId = sizeof(byte);
+
+        public byte Length { get; private set; }
+        public TerrPacketType Type { get; private set; }
+        public byte[] Payload { get; private set; }
+
+        private TerrPacket()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets a connect packet for Terraria156
+        /// </summary>
+        public static byte[] ConnectPacket => new byte[] { 0x0f, 0x00, 0x01, 0x0b, 0x54, 0x65, 0x72, 0x72, 0x61, 0x72, 0x69, 0x61, 0x31, 0x35, 0x36 };
+
+        public static TerrPacket Parse(byte[] data)
+        {
+            byte size = data[Index_Length];
+
+            if (data.Length != size)
+                return null;
+            //if the lenght of the data array isin't the same as the one we got from the array data then it's probably a malformed packet.
+
+            TerrPacket retval = new TerrPacket
+            {
+                Length = size,
+                Type = (TerrPacketType)data[Index_PacketId]
+            };
+
+            //extract payload
+            byte[] payloadBuffer = new byte[size - Index_Payload];
+            Buffer.BlockCopy(data, Index_Payload, payloadBuffer, 0, payloadBuffer.Length);
+            retval.Payload = payloadBuffer;
+
+            return retval;
+        }
+
+        //public static byte[] Create(TerrPacketType type, string stingPayload)
+        //    => Create(type, Encoding.ASCII.GetBytes(stingPayload));
+
+        /// <summary>
+        /// Creates bytes that represent a terraria client -> server (with payload) packet.
+        /// </summary>
+        /// <param name="type">The type of the packet.</param>
+        /// <param name="payload">The payload, which we will be sending to the server.</param>
+        /// <returns>A new byte array containing the bytes of the packet.</returns>
+        public static byte[] Create(TerrPacketType type, byte[] payload)
+        {
+            //Create payloadless packet
+            byte[] payloadLessPacket = Create(type);
+
+            ushort packetLength = Convert.ToUInt16(payloadLessPacket.Length + payload.Length);
+            byte[] packet = new byte[packetLength];
+
+            //Copy over the stuff over from the payload less packet into our new one.
+            Buffer.BlockCopy(payloadLessPacket, 0, packet, 0, payloadLessPacket.Length);
+
+            //Set the new length
+            Buffer.BlockCopy(BitConverter.GetBytes(packetLength), 0, packet, 0, Size_Length);
+
+            //Set payload
+            Buffer.BlockCopy(payload, 0, packet, Index_Payload, payload.Length);
+
+            return packet;
+        }
+
+        /// <summary>
+        /// Creates bytes that represent a payloadless terraria client -> server packet.
+        /// </summary>
+        /// <param name="type">The type of the packet.</param>
+        /// <returns>A new byte array containing the bytes of the packet.</returns>
+        public static byte[] Create(TerrPacketType type)
+        {
+            Console.WriteLine($"Creating packet {type}");
+
+            ushort packetLength = Size_Length + Size_PacketId;
+            byte[] packet = new byte[packetLength];
+
+            //set the length of the packet.
+            Buffer.BlockCopy(BitConverter.GetBytes(packetLength), 0, packet, 0, Size_Length);
+
+            //set the type
+            packet[Index_PacketId] = (byte)type;
+
+            if (type == TerrPacketType.ConnectRequest)
+                //ConenctRequest type packets always have their third byte set to 0x0b
+                packet[3] = 0x0b;
+
+            return packet;
+        }
+    }
+}
